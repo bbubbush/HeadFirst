8e1e51f81c32322bf7d0b0c8208445f45a41c471
bbubbush <bbubbush@gmail.com>
2018-04-05 19:27:27 +0900

    2018-04-05

--- 06_Command/java/RemoteLoader.java
+++ 06_Command/java/RemoteLoader.java
@@ -10,8 +10,26 @@ public class RemoteLoader {
 		CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
 		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
 		
+		
+		Light light = new Light("Living Room");
+		Stereo stereo = new Stereo("Living Room");
+		
+		LightOnCommand lightOn = new LightOnCommand(light);
+		StereoOnWithCDCommand stereoOn = new StereoOnWithCDCommand(stereo);
+		LightOffCommand lightOff = new LightOffCommand(light);
+		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+		
+		Command[] partyOn = {lightOn, stereoOn};
+		Command[] partyOff = {lightOff, stereoOff};
+		
+		MacroCommand partyOnMacro = new MacroCommand(partyOn);
+		MacroCommand partyOffMacro = new MacroCommand(partyOff);
+		
+		
+		
 		remote.setCommand(0, ceilingFanMedium, ceilingFanOff);
 		remote.setCommand(1, ceilingFanLow, ceilingFanOff);
+		remote.setCommand(2, partyOnMacro, partyOffMacro);
 		
 		remote.onButtonWasPushed(0);
 		remote.offButtonWasPushed(0);
@@ -19,6 +37,10 @@ public class RemoteLoader {
 		remote.undoButtonWasPushed();
 		remote.onButtonWasPushed(1);
 		System.out.println(remote);
+		remote.undoButtonWasPushed();
+		System.out.println(remote);
+		remote.onButtonWasPushed(2);
+		remote.offButtonWasPushed(2);
 		remote.undoButtonWasPushed();
 //		
 		
